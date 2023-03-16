341fce9004d7b014c094a8b0e7131922d39e4d60
Alexander-Puv <sansano4sana@gmail.com>
2023-03-16 21:14:23 +0300

    base popup

--- src/components/UI/Popup/Popup.tsx
+++ src/components/UI/Popup/Popup.tsx
@@ -3,8 +3,9 @@ import { ReactNode } from "react"
 interface PopupContentProps {
   title: string,
   content: ReactNode,
-  close: {
+  close?: {
     text: string,
+    disabled?: boolean,
     onClick?: () => void
   }
 }
@@ -15,74 +16,58 @@ interface PopupProps {
   popupContent: PopupContentProps
 }
 
-const Popup = ({isOpen, setIsOpen, popupContent}: PopupProps) => {
+const Popup = ({isOpen, setIsOpen, popupContent: {close, content, title}}: PopupProps) => {
   return <>
-    {isOpen && 
-      <div className="fixed z-10 inset-0 overflow-y-auto">
-        <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-          <div className="fixed inset-0 transition-opacity" onClick={() => setIsOpen(false)}>
-            <div className="absolute inset-0 bg-[black] opacity-75"></div>
-          </div>
-          <span
-            className="hidden sm:inline-block sm:align-middle sm:h-screen"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
-          <div
-            className="inline-block align-bottom
-              bg-white rounded-lg text-left overflow-hidden shadow-xl
-              transform transition-all
-              sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-            role="dialog"
-            aria-modal="true"
-            aria-labelledby="modal-headline"
-          >
-            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-              <div className="sm:flex sm:items-start">
-
-                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                  <h3
-                    className="text-lg leading-6 font-medium text-gray-900"
-                    id="modal-headline"
-                  >
-                    {popupContent.title}
-                  </h3>
-
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      {popupContent.content}
-                    </p>
-                  </div>
-                </div>
+    <div className={`fixed inset-0 overflow-y-auto ${isOpen ? 'opacity-100 z-10' : 'opacity-0 -z-10'} transition-all ease-linear`}>
+      <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div className="fixed inset-0 transition-opacity" onClick={() => setIsOpen(false)}>
+          <div className="absolute inset-0 bg-black opacity-75" />
+        </div>
+        <span className="hidden sm:inline-block sm:align-middle sm:h-screen" />
+        <div
+          className="inline-block align-bottom
+            bg-white rounded-lg text-left overflow-hidden shadow-xl
+            transform transition-all
+            sm:my-8 sm:align-middle sm:max-w-lg sm:w-full
+            dark:bg-gray-700"
+        >
+          <div className="p-4">
+            <div className="sm:flex sm:items-start">
+              <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                <h3 className="text-xl leading-6 font-medium text-gray-900 dark:text-gray-100">
+                  {title}
+                </h3>
+                <p className="text-xs text-gray-600 dark:text-gray-500">
+                  {content}
+                </p>
               </div>
             </div>
-            <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+          </div>
+          {close &&
+            <div className="bg-gray-100 p-4 sm:flex sm:flex-row-reverse dark:bg-gray-800">
               <button
-                className="w-full inline-flex justify-center  px-4 py-2
-                  bg-brand text-base font-medium text-white hover:bg-blue-700
-                  rounded-md border border-transparent shadow-sm
+                className={`button bg-brand rounded-[3px] text-white
                   transition-all ease-linear
-                  active:bg-blue-800
-                  sm:ml-3 sm:w-auto sm:text-sm"
+                  ${close.disabled ? 'text-opacity-50 cursor-default'
+                  : 'hover:bg-blue-700 active:bg-blue-800'}`}
                 onClick={() => {
-                  popupContent.close.onClick && popupContent.close.onClick();
-                  setIsOpen(false)
-                }}
+                  close.onClick && close.onClick();
+                  setIsOpen(false)}}
+                disabled={close.disabled}
               >
-                {popupContent.close.text}
+                {close.text}
               </button>
               <button
-                className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                className="button text-gray-600 text-opacity-80 hover:underline dark:text-white"
                 onClick={() => setIsOpen(false)}
               >
                 Cancel
               </button>
             </div>
-          </div>
+          }
         </div>
       </div>
-    }
+    </div>
   </>
 }
 
